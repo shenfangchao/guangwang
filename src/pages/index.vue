2625881a605e91a1d1c3e49d8b2f6eeb812ee2d9
shenfangchao <2382888481@qq.com>
2024-06-19 17:53:54 +0800

    12

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,7 +11,6 @@ defineOptions({
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
         lackorondy@gmail.com
-        <!-- 639293382@qq.com -->
       </div>
     </div>
   </div>
