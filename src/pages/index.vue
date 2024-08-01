3003894d8fff7ed111155842a469aa039736e896
shenfangchao <2382888481@qq.com>
2024-08-01 17:58:16 +0800

    修改官网

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        sunxiuqing1019@icloud.com
+        sunfuchao0325@icloud.com
       </div>
     </div>
   </div>
