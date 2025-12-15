40ce3756a4adfb24c9618a7c7c05b8459cde2755
shenfangchao <2382888481@qq.com>
2025-12-15 18:59:01 +0800

    lumocall

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        Metti4897165186@icloud.com
+        LumoCall798456@icloud.com
       </div>
     </div>
   </div>
--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const appName = 'Metti'
+const appName = 'LumoCall'
 </script>
 
 <template>
