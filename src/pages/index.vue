99bc9992b3bde90f2c35dc27e624b8bbb97c0216
shenfangchao <2382888481@qq.com>
2026-07-20 14:07:48 +0800

    Hioya

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,8 +6,6 @@ defineOptions({
 
 <template>
   <div relative min-h-100vh>
-     
-
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
         Hioya956187@icloud.com
